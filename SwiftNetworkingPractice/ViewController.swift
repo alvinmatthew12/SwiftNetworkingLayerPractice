6949902f15c9c8d13311f4def0d963a63e9cac26
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2021-04-21 21:22:39 +0700

    Initial Commit

---
+++ SwiftNetworkingPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNetworkingPractice
+//
+//  Created by Alvin Matthew Pratama on 21/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
